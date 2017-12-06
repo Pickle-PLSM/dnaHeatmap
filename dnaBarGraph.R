64bfa7f6f4e52dba960158676bfb9b0e9841acf1
SMCCoder <sofie.christie@gmail.com>
2017-12-05 19:13:25 -0500

    made changes to bar graph

--- dnaBarGraph.R
+++ dnaBarGraph.R
@@ -1,3 +1,4 @@
+# Author: Sofie Christie
 
 # Fetch command line arguments
 myArgs <- commandArgs(trailingOnly = TRUE)
@@ -5,18 +6,18 @@ myArgs <- commandArgs(trailingOnly = TRUE)
 # Convert to numerics
 nums = as.numeric(myArgs)
 
-# cat will write the result to the stdout stream
-cat(nums[1])
+pdf(file="plot.pdf")
 
-pdf(file="myplot.pdf")
+#vector1 <- c(nums[1], nums[2], nums[3], nums[4])
+#barplot(vector1, names.arg=c("A","T","C","G"))
 
-vector <- c(1, 6, 2, 1)
-barplot(vector, names.arg=c("A","T","C","G"))
+#vector2 <- c(2, 8, 18, 1)
+#barplot(vector2, names.arg=c("A","T","C","G"))
 
-#graph(nums[1], nums[2], nums[3], nums[4])
+graph <- function(ACount, TCount, CCount, GCount) {
+  vector <- c(ACount, TCount, CCount, GCount)
+  barplot(vector, names.arg=c("A","T","C","G"), col=rainbow(4), main="ATCG Content", xlab="Nucleotide Bases", ylab="Count", xpd=FALSE, ylim=c(0,max(nums)+5))
+}
 
-#graph <- function(ACount, TCount, CCount, GCount) {
-#  vector <- c(ACount, TCount, CCount, GCount)
-#  barplot(vector, names.arg=c("A","T","C","G"))
-#}
-
+graph(nums[1], nums[2], nums[3], nums[4])
+graph(1, 2, 5, 3)
--- testpython.py
+++ testpython.py
@@ -1,18 +1,18 @@
-# run_max.py
+# run testpython.py
 import subprocess
 
 # Define command and arguments
 command = 'Rscript'
 path2script = 'dnaBarGraph.R'
 
+def build(args):
+    # Build subprocess command
+    cmd = [command, path2script] + args
+
+    # check_output will run the command and store to result
+    subprocess.check_output(cmd, universal_newlines=True)
+
 # Variable number of args in a list
-args = ['11', '3', '9', '42']
+seqcount1 = [['2', '7', '9', '1'],['1', '15', '5', '10'],['6', '2', '9', '13']]
 
-# Build subprocess command
-cmd = [command, path2script] + args
-
-# check_output will run the command and store to result
-x = subprocess.check_output(cmd, universal_newlines=True)
-
-print(x)
-# print('The maximum of the numbers is:', x)
+build(seqcount1[0])
