e7e83ac112102adac876aaf025c2b3e42ae532b6
Pnips <pxr8793@g.rit.edu>
2017-12-06 00:32:27 -0500

    pipes data from python to R, heatmap settings might need tweaking

--- Project.py
+++ Project.py
@@ -1,27 +1,47 @@
-'''
-@author Lyndsey Allison
-    lines 10-
-Introduction to Bioinformatics
-Fall 2017
-Final Project
-Input files in python
-'''
-#use a while loop so that the user can input as many files as they want
-while True:
-    #tells python that it is opening a file
-    def file(filename):
+import csv
+import subprocess
 
-        with open(filename):
-            raw_input('file: ')
-    answer = raw_input("Insert a filename or insert quit once done adding files: ")
+Acounter = 0
+Gcounter = 0
+Ccounter = 0
+Tcounter = 0
+command = 'Rscript'
+path2script = 'dnaSeqHeatmap.R'
+
+with open('SeqComparison.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    #spamwriter.writerow([''] + ['A'] + ['G'] + ['C'] + ['T'])
+    spamwriter.writerow(['','A','G','C','T'])
+    while True:
+    #tells python that it is opening a file
+        def file(filename):
+
+            with open(filename):
+                raw_input('file: ')
+        answer = raw_input("Insert a filename or insert quit once done adding files: ")
     #there are two possibilities for the user to input a file or quit if the user inputs one thing it will do one thing if they input something else it will do something else
-    if answer != 'quit':
+        if answer != 'quit':
         #if the file inputted by the user (answer) is not quit it will open the file with the permission to read
-        with open(answer, "r") as file:
-		data = file.read().replace('\n', '') 
+            with open(answer, "r") as file:
+	    	data = file.read().replace('\n', '') 
 		print data
         #print is used to check to see if the file was open in the with the permissions to read is not needed now
         #print file
-    else:
-        #if the user inputs quit then it will break out of the loop
-        break
+            for index in range ( (len(data)) - 1):
+                if data[index] == 'A':
+                    Acounter += 1
+                if data[index] == 'G':
+                    Gcounter += 1
+                if data[index] == 'C':
+                    Ccounter += 1
+                if data[index] == 'T':
+                    Tcounter += 1
+            spamwriter.writerow([answer,str(Acounter),str(Tcounter),str(Ccounter),str(Gcounter)])
+        else:
+            def build():
+                cmd = [command, path2script]
+                subprocess.check_output(cmd, universal_newlines=True)
+
+            build()
+            break
--- dnaSeqHeatmap.R
+++ dnaSeqHeatmap.R
@@ -1,8 +1,8 @@
+#! /usr/bin/Rscript
+
 ## Author: Patrick Rynkiewicz
 ## Description: Visualization of DNA Content via heatmap.
 
-# set the working directory (this is where the input files are and where the output will go).
-# setwd("/home/pmoney/Desktop")
 
 # read in input file.
 seqs <- read.csv('SeqComparison.csv', header = T)
@@ -15,4 +15,5 @@ seq_matrix <- data.matrix(seqs[ , -1]) #make the numbers in the file numeric dat
 # plot heatmap.
 png(filename = "heatmap_output.png") #open png file.
 heatmap(seq_matrix, Rowv=NA, Colv=NA,  scale="column", margins=c(7,10)) #heatmap plot.
-dev.off() #close the png file.
+dev.off() 
+#close the png file.
