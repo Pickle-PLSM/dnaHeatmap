2f6e9c351437df36c6473a4e8314f438f4034b5a
Pnips <pxr8793@g.rit.edu>
2017-12-07 14:37:41 -0500

    fixed dnaSeqHeatmap so it uses local directory

--- Init.py
+++ Init.py
@@ -1,24 +1,26 @@
+#Authors: Patrick Rynkiewicz, Sofie Christie, Melody Kabbai, and Lyndsey Allison
 import csv
 import subprocess
 
 # Define command and arguments
-command = 'Rscript'
-path2script = 'dnaSeqHeatmap.R'
+command = 'Rscript' # type of script for subprocess to run
+path2script = ''  # will become path to script user selects
 
+#counters for possible nucleotide bases
 Acounter = 0
 Gcounter = 0
 Ccounter = 0
 Tcounter = 0
 
 # tells whether or not to run the subprocess
-graph = False;
+graph = False
 
-with open('SeqComparison.csv', 'wb') as csvfile:
+with open('SeqComparison.csv', 'wb') as csvfile:    #following will be written to a csv
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(["File","A","G","C","T"])
+    spamwriter.writerow(["File","A","G","C","T"])     #the header (first row) will denote bases
 
-    rfile = raw_input("Do you want a heatmap or a bargraph? ")
+    rfile = raw_input("Do you want a heatmap or a bargraph? ") #user choice of formatted output
 
     if(rfile == "heatmap"):
         path2script = "dnaSeqHeatmap.R"
@@ -33,16 +35,15 @@ with open('SeqComparison.csv', 'wb') as csvfile:
             with open(filename):
                 raw_input('file: ')
 
-        answer = raw_input("Insert a filename or insert quit once done adding files: ")
-        #there are two possibilities for the user to input a file or quit if the user inputs one thing it will do one thing if they input something else it will do something else
+        answer = raw_input("Insert a filename or insert quit once done adding files: ") #process text file name input or exit while loop if user selects quit
         if answer != 'quit':
         #if the file inputted by the user (answer) is not quit it will open the file with the permission to read
             with open(answer, "r") as file:
-               data = file.read().replace("\n", "")
+               data = file.read().replace("\n", "")    #get rid of those pesky newline characters and save input as a string
                print(data)
         #print is used to check to see if the file was open in the with the permissions to read is not needed now
         #print file
-            for index in range ( (len(data)) - 1):
+            for index in range ( (len(data)) - 1):   #data analysis, nucleotide counters will increase based on string content
                 if data[index] == 'A':
                     Acounter += 1
                 if data[index] == 'G':
@@ -51,16 +52,18 @@ with open('SeqComparison.csv', 'wb') as csvfile:
                     Ccounter += 1
                 if data[index] == 'T':
                     Tcounter += 1
-            spamwriter.writerow([answer,str(Acounter),str(Tcounter),str(Ccounter),str(Gcounter)])
+            spamwriter.writerow([answer,str(Acounter),str(Tcounter),str(Ccounter),str(Gcounter)])   #write new row to csv when text file of question is processed
         else:
             # after finished creating csv, graph equals true
             graph = True
             break
 
-# call the R script to make the graph
+# call the R script to make the graph/heatmap
 if graph is True:
     # Build subprocess command
     cmd = [command, path2script]
 
     # check_output will run the command and store to result
     subprocess.check_output(cmd, universal_newlines=True)
+
+#blessed
--- dnaSeqHeatmap.R
+++ dnaSeqHeatmap.R
@@ -2,8 +2,7 @@
 
 ## Author: Patrick Rynkiewicz
 ## Description: Visualization of DNA Content via heatmap.
-
-
+#Author: Patrick Rynkiewicz
 # read in input file.
 seqs <- read.csv('SeqComparison.csv', header = T)
 
@@ -15,5 +14,5 @@ seq_matrix <- data.matrix(seqs[ , -1]) #make the numbers in the file numeric dat
 # plot heatmap.
 png(filename = "heatmap_output.png") #open png file.
 heatmap(seq_matrix, Rowv=NA, Colv=NA,  scale="column", margins=c(7,10)) #heatmap plot.
-#dev.off() 
+dev.off() 
 #close the png file.
